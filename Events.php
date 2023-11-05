9c55a2e24e3441bad672f01a5efd588e45573d5f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-11-05 14:28:57 -0500

    Update Events.php

--- Events.php
+++ Events.php
@@ -49,17 +49,17 @@ class Events
             foreach ( $ccmsEnabled as $ccms ) {
                 $cc = ContentContainer::findOne($ccms->contentcontainer_id);
                 $space = Space::findOne($cc->pk);
-                $interval= $space->getSetting('interval', 'rss', 60);
-                $lastrun= $space->getSetting('lastrun', 'rss', '');
+                $interval = $this->settings->space()->get('interval', 'rss', 60);
+                $lastrun = $this->settings->space()->get('lastrun', 'rss', '');
                 if (! empty($lastrun) && time() < ($interval * 60 + $lastrun))
                     continue;
-                $space->setSetting('lastrun', time(), 'rss');
+                $space->$this->settings->space()->set('lastrun', time(), 'rss');
                 Console::stdout("  Queueing update for space \"" . $space->name . "\"\n");
                 Yii::$app->queue->push(new jobs\GetFeedUpdates(['space' => $space, 'force' => false]));
             }
             Console::stdout(Console::renderColoredString("%gdone.%n\n", 1));
         } catch (\Throwable $e) {
-            $event->sender->stderr($e->getMessage()."\n");
+            $e->getMessage()."\n";
             Yii::error($e);
         }
     }
