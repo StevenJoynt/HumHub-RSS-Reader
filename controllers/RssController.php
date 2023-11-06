607a74411d2a5c349c2406ab0c95d5192bdfe858
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-11-06 14:15:14 -0500

    Update RssController.php

--- controllers/RssController.php
+++ controllers/RssController.php
@@ -29,4 +29,23 @@ class RssController extends ContentContainerController
         return $this->render('config', ['model' => $model]);
     }
 
+    public static function vetOwner($owner, $space) {
+        $owner = explode(',', $owner)[0]; // only interested in the first owner
+        $user = null;
+        if ( $owner ) {
+            if ( ctype_digit($owner) ) {
+                // user id number supplied - from our settings
+                $user = User::findOne(['id'=>$owner]);
+            } else {
+                // user guid string supplied - from UserPicker::widget
+                $user = User::findOne(['guid'=>$owner]);
+            }
+        }
+        if ( ! $user ) {
+            // no valid user selected
+            // default to the owner of this space
+            $user = $space->ownerUser;
+        }
+        return $user;
+    }
 }
